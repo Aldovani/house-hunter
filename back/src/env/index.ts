2d1fbb1bf5100dfb2c3cdf62b68929c281eb7b9d
Aldovani <aldovanihcosta@gmail.com>
2023-07-11 00:50:23 -0300

    chore: add new dependencies and environment variables

--- back/src/env/index.ts
+++ back/src/env/index.ts
@@ -4,6 +4,8 @@ import { z } from 'zod'
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string(),
+  JWT_SECRET_ACCESS: z.string(),
+  JWT_SECRET_REFRESH: z.string(),
   MAIL_TRAP_HOST: z.string(),
   MAIL_TRAP_PORT: z.coerce.number(),
   MAIL_TRAP_AUTH_USER: z.string(),
